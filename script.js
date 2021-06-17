af7894fdc9f7857857e6d008e9d89cc23a1a12f0
ajin16 <56349512+ajin16@users.noreply.github.com>
2021-06-17 15:50:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if(principal.value==0)
+    {
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+
+    if(rate.value == "")
+    {
+        alert("Please enter the Rate");
+        rate.focus();
+        return false;
+    }
+
+    if(years.value == "")
+    {
+        alert("Please enter the Time in years");
+        year.focus();
+        return false;
+    }
+
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerText=interest;
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
